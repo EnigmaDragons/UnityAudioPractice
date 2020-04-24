7d2b44e4673bdd37e4ed59d9dbfdd02f23b4dde7
SilasReinagel <silas.reinagel@gmail.com>
2020-04-24 13:05:29 -0700

    Initial Project Commit

---
+++ src/UnityAudioPractice/Assets/Scripts/Audio/MixerVolumeSlider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public sealed class MixerVolumeSlider : MonoBehaviour 
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider slider;
+    [SerializeField] private string valueName = "MusicVolume";
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat(valueName, 0.5f);
+        slider.onValueChanged.AddListener(SetLevel);
+    }
+    
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat(valueName, (Mathf.Log10(sliderValue) * 20));
+        PlayerPrefs.SetFloat(valueName, sliderValue);
+    }
+}
---
+++ src/UnityAudioPractice/Assets/Scripts/Navigator.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class Navigator : ScriptableObject
+{
+    public void NavigateToScene1() => LoadScene("Scene1");
+    public void NavigateToScene2() => LoadScene("Scene2");
+
+    private void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+}
